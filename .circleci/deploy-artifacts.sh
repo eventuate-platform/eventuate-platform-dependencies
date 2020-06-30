1b341dbec9fe8b0589fa7441188fbe1d53240419
Chris Richardson <chris@chrisrichardson.net>
2020-06-30 09:02:43 -0700

    Configured CircleCI

---
+++ .circleci/deploy-artifacts.sh
@@ -0,0 +1,33 @@
+#! /bin/bash -e
+
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [  $BRANCH == "master" ] ; then
+  BUILD_SNAPSHOT=$(grep '<version>' pom.xml  | head -1 | sed -e 's/.*<version>//' -e 's/<.*//' -e 's/-SNAPSHOT/.BUILD-SNAPSHOT/')
+  echo master: publishing $BUILD_SNAPSHOT
+  ./mvnw versions:set -D newVersion=$BUILD_SNAPSHOT
+  ./mvnw deploy -D deploy.repo=${S3_REPO_DEPLOY_URL?}  -D version=0.1.0.BUILD-SNAPSHOT
+  exit 0
+fi
+
+if ! [[  $BRANCH =~ ^[0-9]+ ]] ; then
+  echo Not release $BRANCH - no PUSH
+  exit 0
+elif [[  $BRANCH =~ RELEASE$ ]] ; then
+  BINTRAY_REPO_TYPE=release
+elif [[  $BRANCH =~ M[0-9]+$ ]] ; then
+    BINTRAY_REPO_TYPE=milestone
+elif [[  $BRANCH =~ RC[0-9]+$ ]] ; then
+    BINTRAY_REPO_TYPE=rc
+else
+  echo cannot figure out bintray for this branch $BRANCH
+  exit -1
+fi
+
+echo BINTRAY_REPO_TYPE=${BINTRAY_REPO_TYPE}
+
+VERSION=$BRANCH
+
+echo Implement me
+
+exit 99
