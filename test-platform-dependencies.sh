16ee3540c20122504161194af7dc7cff7574f72d
Chris Richardson <chris@chrisrichardson.net>
2025-03-12 19:44:46 +0900

    Added eventuate-tram-spring-testing-support-cloud-contract-bom

--- test-platform-dependencies.sh
+++ test-platform-dependencies.sh
@@ -3,6 +3,6 @@
 VERSION=$(date '+%Y%m%d%H%M%S')
 DEPLOY_DIR=$(pwd)/build/test-deploy
 
-./gradlew -P version=$VERSION -P deployUrl=file://$DEPLOY_DIR publish
+./gradlew --parallel -P version=$VERSION -P deployUrl=file://$DEPLOY_DIR publish
 
-./gradlew -P version=$VERSION -P eventuateMavenRepoUrl=file://$DEPLOY_DIR,https://snapshots.repositories.eventuate.io/repository --build-file build-example.gradle assemble
+./gradlew --parallel -P version=$VERSION -P eventuateMavenRepoUrl=file://$DEPLOY_DIR,https://snapshots.repositories.eventuate.io/repository --build-file build-example.gradle assemble
